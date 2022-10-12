96aa078bfce5684d7e13a262ab0b6fdc2a2d3b07
michael <chalamaicol9@gmail.com>
2022-10-11 22:28:15 -0500

    vista con el menu login y plantila inicio

---
+++ js/eventos.js
@@ -0,0 +1,63 @@
+ function mostrarComputadores(){
+console.log("desde jquery");
+	$.ajax({    
+	    url : 'http://localhost:8080/api/Computer/all',
+	    type : 'GET',
+	    dataType : 'json',
+		success:function(computadores){
+		let datoscomputadores ="";
+		for (i=0; i<computadores.length; i++){
+			datoscomputadores = "<tr>";
+			datoscomputadores += "<td>"+computadores[i].id+"</td>";
+			datoscomputadores += "<td>"+computadores[i].name+"</td>";
+			datoscomputadores += "<td>"+computadores[i].brand+"</td>";
+			datoscomputadores += "<td>"+computadores[i].year+"</td>";
+			datoscomputadores += "<td>"+computadores[i].description+"</td>";
+			datoscomputadores += "<td><a href = '#' onclick = 'editar("+computadores[i].id+");' data-bs-toggle='modal' data-bs-target='#modalEditar' class = 'btn btn-warning'>Editar</a></td>";
+			datoscomputadores += "<td><a href = '#' onclick = 'eliminar("+computadores[i].id+");' class = 'btn btn-danger'>Eliminar</a></td>";
+			datoscomputadores += "</tr>";
+
+			$("#listaComputadores").append(datoscomputadores);
+		}
+
+		
+	}
+
+	});
+
+}
+/** 
+
+$.ajax({    
+	url : 'http://localhost:8080/api/Computer/all',
+	type : 'GET',
+	dataType : 'json',
+	success:function(computadores){
+	let datoscomputadores ="";
+	let id;
+	let nombre =$("#nombre");
+	let brand=$("#brand");
+	let year=$("#year");
+	let description=$("#description");
+	
+
+	for (i=0; i<computadores.length; i++){
+		id = +computadores[i].id;
+		nombre =computadores[i].name;
+		brand = computadores[i].brand;
+		year =computadores[i].year;
+		description =computadores[i].description;
+
+
+		$("#listaComputadores").append(datoscomputadores);
+	}
+
+	
+}
+
+});
+
+**/
+
+
+
